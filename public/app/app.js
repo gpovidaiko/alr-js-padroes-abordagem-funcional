08f0e55a11496acbb1c983ff592153797969a65f
gpovidaiko <gpovidaiko@hotmail.com>
2023-05-21 23:22:37 -0300

    05. Lidando com problemas de rede.
    Criação da função auxiliar de promises timeoutPromise, que serve para
    criar uma promise de rejeição, que por concorrência, será resolvida se a
    promise principal não for concluída até um dado tempo em milissegundos.
    
    Criação de função auxiliar de promises delay, que serve para postergar a
    resolução de uma promise após uma dada quantidade de tempo em
    milissegundos.
    
    Criação da função auxiliar de promises retry, que serve para retentar a
    promise principal sempre que houver uma rejeição, aguardando um dado
    tempo e até atingir uma dada quantidade de vezes e se ainda assim houver
    rejeição, permite seguir adiante.

--- public/app/app.js
+++ public/app/app.js
@@ -1,6 +1,7 @@
 import { notasSercive } from "./nota/service.js";
 import './utils/array-helpers.js';
 import { debounceTime, partialize, pipe, takeUntil } from "./utils/operators.js";
+import { retry, timeoutPromise } from "./utils/promise-helpers.js";
 
 const operations = pipe(
 	partialize(takeUntil, 3),
@@ -8,8 +9,15 @@ const operations = pipe(
 );
 
 const action = operations(
-	() => notasSercive
-		.sumItems('2143')
+	() => retry(
+			3,
+			3000,
+			() => 
+			timeoutPromise(
+				200, 
+				notasSercive.sumItems('2143')
+			) 
+		)
 		.then(console.log)
 		.catch(console.log)
 )
--- public/app/utils/promise-helpers.js
+++ public/app/utils/promise-helpers.js
@@ -5,3 +5,25 @@ export const log = res => {
 	console.log(res);
 	return res;
 }
+
+export const timeoutPromise = (milliseconds, promise) => {
+	const timeoutPromise = new Promise((resolve, reject) => 
+		setTimeout(() => reject(`Limite da promise excedido (limite: ${milliseconds} ms)`), milliseconds)
+	);
+	return Promise.race([promise, timeoutPromise]);
+}
+
+export const delay = milliseconds => data => 
+	new Promise(
+		(resolve, reject) => setTimeout(() => resolve(data), milliseconds)
+	)
+
+export const retry = (retries, milliseconds, fn) => 
+	fn().catch(err => {
+		console.log(retries);
+		return delay(milliseconds)().then(() =>
+			retries > 1 
+				? retry(retries - 1, milliseconds, fn) 
+				: Promise.reject(err)
+		)
+	})
