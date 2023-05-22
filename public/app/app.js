0ba84dbb5eaaeeb0375c57bd8379033fd93bd580
gpovidaiko <gpovidaiko@hotmail.com>
2023-05-21 22:26:04 -0300

    04. Adequação da interação do usuário.
    Implementação do opedaror takeUntil, para executar uma operação um
    determinado número de vezes.
    
    Implementação do operador debounceTime, para executar uma operação após
    um determinado tempo em milissegundos.
    
    Visto a influência do conceito de 'closure' para o funcionamento desses
    operadores.

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,19 @@
 import { notasSercive } from "./nota/service.js";
 import './utils/array-helpers.js';
+import { debounceTime, partialize, pipe, takeUntil } from "./utils/operators.js";
+
+const operations = pipe(
+	partialize(takeUntil, 3),
+	partialize(debounceTime, 500)
+);
+
+const action = operations(
+	() => notasSercive
+		.sumItems('2143')
+		.then(console.log)
+		.catch(console.log)
+)
 
 document
-.querySelector('#myButton')
-.onclick = () =>
-	notasSercive
-	.sumItems('2143')
-	.then(console.log)
-	.catch(console.log);
+	.querySelector('#myButton')
+	.onclick = action;
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -6,3 +6,14 @@ export const compose = (...fns) =>
 
 export const pipe = (...fns) => 
 	value => fns.reduce((previous, fn) => fn(previous), value);
+
+export const takeUntil = (times, fn) =>
+	() => times-- > 0 && fn();
+
+export const debounceTime = (milliseconds, fn) => {
+	let timer = 0;
+	return () => {
+		clearTimeout(timer);
+		timer = setTimeout(fn, milliseconds);
+	}
+}
